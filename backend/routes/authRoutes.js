8a46a312e540b3aebdee07604999113825059649
vasundhara2509 <vasundhara50758@gmail.com>
2026-05-08 08:48:47 +0530

    Fixed user model import case sensitivity

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 const asyncHandler =
   require('express-async-handler');
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 
 // GENERATE TOKEN
